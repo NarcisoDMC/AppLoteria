5d1733aef4b4e97ae350e94b37d98d6211d9f685
NarcisoDMC <davidmadefukingtroll810@gmail.com>
2025-03-12 13:47:19 -0600

    Cambios dia 12 de marzo del 2025

--- app/src/main/java/com/example/apploteria/views/LoteriaView.kt
+++ app/src/main/java/com/example/apploteria/views/LoteriaView.kt
@@ -1,12 +1,28 @@
 package com.example.apploteria.views
 
+import android.R
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.apploteria.ViewModels.LoteriaViewModel
 
+@Composable
 fun LoteriaView(viewModel: LoteriaViewModel){
     val lottoNumbers = viewModel.lottoNumbers.value
 
@@ -15,13 +31,14 @@ fun LoteriaView(viewModel: LoteriaViewModel){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        if (lottoNumbers.isEmpty){
-            Text(text = "Loteria", fontSize = 40.sp, fontWeight = fontWeight.Bold)
+        if (lottoNumbers.isEmpty()){
+            Text(text = "Loteria", fontSize = 40.sp, fontWeight = FontWeight.Bold)
         }else{
             LotteryNumbers(numbers = lottoNumbers)
         }
+
         Button(onClick = { viewModel.generateLottoNumbers() }){
-            Text(text = "Generar", fontSize = 20.sp, fontWeight = fontWeight.Bold)
+            Text(text = "Generar", fontSize = 20.sp, fontWeight = FontWeight.Bold)
         }
     }
 }
